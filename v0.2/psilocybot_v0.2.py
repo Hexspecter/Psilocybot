a338e41557310aa6be6ee4b94be745b1c6d5f8ea
Hexspecter <78763441+Hexspecter@users.noreply.github.com>
2021-03-15 18:18:58 +0200

    Update psilocybot_v0.2.py

--- v0.2/psilocybot_v0.2.py
+++ v0.2/psilocybot_v0.2.py
@@ -16,6 +16,7 @@ bot = commands.Bot(command_prefix='>', help_command=None)
 async def help(ctx):
     embed=discord.Embed(title="Commands and info", description="Information about the different commands and arguments", color=0xff00ff)
     embed.add_field(name="Information", value="This bot is using the Tripsit.me and Psychonautwiki APIs for searches and requires specific substance names.\nDatabase: https://drugs.tripsit.me/", inline=False)
+    embed.add_field(name=">dose", value="Prettified information for dose and summary."
     embed.add_field(name=">tripsitme", value="Searches info and dosage of a substance from Tripsit.me, aliases: tripsit, ts, tsdose. \nUsage: >tripsitme LSD", inline=True)
     embed.add_field(name=">psychonautwiki", value="Searches info and dosage of a substance from Psychonautwiki, aliases: psychonaut, psych, psw, pswdose. \nUsage: >psychonautwiki LSD", inline=True)
     embed.add_field(name=">funhelp", value="Gives you information about the fun commands, aliases: fh, funguide, funinfo", inline=True)
@@ -66,6 +67,127 @@ async def stealthshot(ctx):
 async def stealthdrink(ctx):
 	await ctx.send(f"{ctx.author.name} is cracking open a cold one, join them and crack open a cold one!")
 
+
+# Mash both together to get even prettier menus!
+@bot.command(aliases=['drug', 'substance'])
+async def dose(ctx, query: str):
+	search = {'name': query}
+	r = requests.get('http://tripbot.tripsit.me/api/tripsit/getDrug', params=search)
+	x = r.json()
+    # Sort JSON and prep vars for pretty embeds
+	for author in x["data"]:
+		desc = author["pretty_name"]
+		prop = author["properties"]
+		dosageas = prop["dose"]
+		summ = prop["summary"]
+	headers = {
+		"accept-type": "application/json",
+		"content-type": "application/json"
+	}
+	# Create JSON post payload
+	payload = {
+	    "query": """
+	{
+	    substances(query: "%s") {
+	        name
+	        summary
+	        # routes of administration
+	        roas {
+	            name
+	            dose {
+	                units
+	                threshold
+	                heavy
+	                common { min max }
+	                light { min max }
+	                strong { min max }
+	            }
+			        }
+		        url
+			    }
+			}
+			""" % query
+		}
+	# Make it into a JSON dump
+	json_payload = json.dumps(payload)
+	# Init API payload for posting
+	api = requests.post("https://api.psychonautwiki.org/?",data=json_payload,headers=headers)
+	# Convert data into json
+	y = api.json()
+	# Sort through the JSON data
+	for subs in y["data"]["substances"]:
+		# Get substance name for embed
+		name = subs["name"]
+		# See if a summary exists (usually empty)
+		# summary = subs["summary"] (We're pulling this from Tripsit.me for this command)
+		# Get link for the wiki page
+		link = subs["url"]
+		# JSON sorting informations from POST
+		doses = subs["roas"][0]["dose"]
+		# Get units
+		units = doses["units"]
+
+		# Get threshold dose info
+		threshold = doses["threshold"]
+		# Generate pretty doses (0-999 mg)
+		thresholdstr = str(threshold)
+		thresholdf = thresholdstr, units
+
+		# Get light dose info
+		light = doses["light"]
+		# Get light dose min and max values as ints
+		lightmin = light["min"]
+		lightmax = light["max"]
+		# Convert ints to strings
+		lightminstr = str(lightmin)
+		lightmaxstr = str(lightmax)
+		# Create a tuple for joining with units info
+		lightuple = (lightminstr, lightmaxstr)
+		lightx = "-".join(lightuple)
+		lighty = lightx, units
+
+		# Get common dose info
+		common = doses["common"]
+		# Get common dose min and max values as ints
+		commonmin = common["min"]
+		commonmax = common["max"]
+		# Convert ints to strings
+		commonminstr = str(commonmin)
+		commonmaxstr = str(commonmax)
+		# Create a tuple for joining with units info
+		commontuple = (commonminstr, commonmaxstr)
+		commonx = "-".join(commontuple)
+		commony = commonx, units
+
+		# Get strong dose info
+		strong = doses["strong"]
+		# Get strong dose min and max values as ints
+		strongmin = strong["min"]
+		strongmax = strong["max"]
+		# Convert ints to strings
+		strongminstr = str(strongmin)
+		strongmaxstr = str(strongmax)
+		# Create a tuple for joining with units info
+		strongtuple = (strongmaxstr, strongminstr)
+		strongx = "-".join(strongtuple)
+		strongy = strongx, units
+
+		# Get heavy dose info
+		heavy = doses["heavy"]
+		# Convert int to str
+		heavystr = str(heavy)
+		heavyf = heavystr, units
+
+	# Create pretty embed
+	embed=discord.Embed(title=name, description=summ, color=0x00ff00)
+	embed.add_field(name="Dosage", value="Average dosages", inline=False)
+	embed.add_field(name="Threshold", value=" ".join(thresholdf), inline=True)
+	embed.add_field(name="Light", value=" ".join(lighty), inline=True)
+	embed.add_field(name="Common", value=" ".join(commony), inline=True)
+	embed.add_field(name="Strong", value=" ".join(strongy), inline=True)
+	embed.add_field(name="Heavy", value=" ".join(heavyf), inline=True)
+	embed.add_field(name="More information", value=link, inline=False)
+	await ctx.send(embed=embed)
 
 # Proper useful commands! Yay!
 # Tripsitme API json get + sorting
