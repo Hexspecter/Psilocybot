865fe638290b33a49ac127140a09bb13921cf829
Hexspecter <78763441+Hexspecter@users.noreply.github.com>
2021-06-28 21:47:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.members = True
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='>', help_command=None, case_insensitive=True, intents=intents)
+
+for filename in os.listdir('./commands'):
+	if filename.endswith('.py'):
+		bot.load_extension(f'commands.{filename[:-3]}')
+	else:
+		print(f'Unable to load {filename[:-3]}')
+
+@bot.event
+async def on_ready():
+	print("Bot is ready!")
+	await bot.change_presence(activity=discord.Game(name="Regular functionality!"))
+
+bot.run(TOKEN)
