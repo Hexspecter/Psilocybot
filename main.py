842f7ee93a5fc554f28dfa0c31a49effced3c0c3
Hexspecter <78763441+Hexspecter@users.noreply.github.com>
2022-01-13 18:56:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import discord
 import os
+import aiohttp
+import asyncio
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -7,7 +9,7 @@ intents = discord.Intents.default()
 intents.members = True
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='>', help_command=None, case_insensitive=True, intents=intents)
+bot = commands.Bot(command_prefix='>', help_command=None, owner_id="Your owner ID", case_insensitive=True, intents=intents)
 
 for filename in os.listdir('./commands'):
 	if filename.endswith('.py'):
@@ -19,5 +21,6 @@ for filename in os.listdir('./commands'):
 async def on_ready():
 	print("Bot is ready!")
 	await bot.change_presence(activity=discord.Game(name="Regular functionality!"))
+
 
 bot.run(TOKEN)
